ba5d70291edee280ecd032c88c33b2b25780300f
Matthias Lenk <matthias.lenk@gmx.de>
2017-08-15 12:27:04 +0200

    rename def config for both variants having the same kernel config
    Change-Id: I268fb511789ec8410dcf68db26e171725a639794

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/lenovo/YTX703F
 TARGET_OTA_ASSERT_DEVICE := yt_x703f
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineage_YTX703F_defconfig
+TARGET_KERNEL_CONFIG := lineage_YTX703_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
